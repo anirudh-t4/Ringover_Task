ce476f09a60a77de7126a48c252ec059e86dd165
ANIRUDH TIWARI <55625660+anirudh-t4@users.noreply.github.com>
2023-02-13 23:38:46 +0530

    Update Cart.jsx

--- src/components/store/Cart.jsx
+++ src/components/store/Cart.jsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { AiOutlineDelete } from "react-icons/ai";
+var tot=0
 
 const Cart = ({ cart, setCart }) => {
 
     const Filled = () => {
+        tot=0
         return (
             <>
                 {cart.map((pro) => {
+                    
+                    tot+=pro.price
                     return (
                         <div className="cart__item" key={pro.id}>
                             <img src={pro.img} alt="" style={{width: '8rem', marginLeft: 0, height: '6rem'}} />
                             <div className="cart__item__info">
                                 <h3>{pro.name}</h3>
-                                <p>Rs. {pro.price}</p>
+                                <p>{pro.price} points</p>
                             </div>
                             <button className="no-style" onClick={() => setCart(cart.filter((item) => {
                                 return item.id !== pro.id
@@ -20,14 +24,19 @@ const Cart = ({ cart, setCart }) => {
                         </div>
                     )
                 })}
+                <br/><br/><br/>
+                <div>
+                <h3>{tot} points</h3>
+                </div>
                 </>
+                
         )
     }
 
   return (
   <div className="cart">
     {
-        cart.length ? <Filled /> : <p>What's stopping you, designer?</p>
+        cart.length ? <Filled /> : <p>What's stopping you, saver?</p>
     }
   </div>
   );
